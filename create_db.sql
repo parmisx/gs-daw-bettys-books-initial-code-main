0c3983268a20a847f8adbc6ede5299f2078b73a9
phoss001 <phoss001@doc.gold.ac.uk>
2024-10-09 22:35:58 +0100

    task 2 of lab 2: registering users

--- create_db.sql
+++ create_db.sql
@@ -7,6 +7,12 @@ USE bettys_books;
 # Create the tables
 CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
 
+# insert a book 
+INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+
+# Create the table for users details
+CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT,username VARCHAR(50) NOT NULL UNIQUE, first_name VARCHAR(50) NOT NULL,last_name VARCHAR(50) NOT NULL,email VARCHAR(100) NOT NULL UNIQUE, hashedPassword VARCHAR(255) NOT NULL, PRIMARY KEY(id));
+
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
 GRANT ALL PRIVILEGES ON bettys_books.* TO ' bettys_books_app'@'localhost';
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,44 @@
 const express = require("express")
 const router = express.Router()
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')                                                               
 })    
 
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email)                                                                           
-})
+
+    const plainPassword = req.body.password;
+    const username = req.body.username;
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const email = req.body.email;
+
+    bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword){
+        if(err){
+            next(err);
+        } else {
+        // saving data in database
+        let sqlquery = "INSERT INTO users(username, first_name, last_name, email, hashedPassword) VALUES (?, ?, ?, ?, ?)";
+        // execute the query
+        let newUser = [username, firstName, lastName, email, hashedPassword];
+
+        db.query(sqlquery, newUser, (err, result)=>{
+            if(err){
+                next(err);
+            } else{                                      
+                //res.send(' Hello '+ req.body.firstName + ' '+ req.body.lastName +' you are now registered!  We will send an email to you at ' + req.body.email);
+                result = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email
+                result += 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword
+                res.send(result)
+                
+            }
+        });
+        }
+    });
+});
 
 // Export the router object so index.js can access it
 module.exports = router

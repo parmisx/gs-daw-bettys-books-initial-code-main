7ec878280f4810a566523e2d947f8b57d2b3cd7d
phoss001 <phoss001@doc.gold.ac.uk>
2024-11-20 20:21:33 +0000

    lab 7 (task 1)

--- create_db.sql
+++ create_db.sql
@@ -4,14 +4,50 @@
 CREATE DATABASE IF NOT EXISTS bettys_books;
 USE bettys_books;
 
-# Create the tables
-CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
+# create the table for authors
+CREATE TABLE IF NOT EXISTS authors (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
+ALTER TABLE books ADD COLUMN author_id INT NOT NULL;
 
-# insert a book 
-INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+# Create the table for books
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(50),
+    price DECIMAL(5, 2) unsigned,
+    author_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE
+);
+ALTER TABLE books
+ADD CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE;
+
+# insert authors
+INSERT INTO authors (name)
+VALUES
+    ('Graham Greene'),
+    ('Aldous Huxley'),
+    ('George Orwell');
+
+# insert books
+INSERT INTO books (name, price, author_id)
+VALUES
+    ('Brighton Rock', 20.25, 1),
+    ('Brave New World', 25.00, 2),
+    ('Animal Farm', 12.99, 3);
 
 # Create the table for users details
-CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT,username VARCHAR(50) NOT NULL UNIQUE, first_name VARCHAR(50) NOT NULL,last_name VARCHAR(50) NOT NULL,email VARCHAR(100) NOT NULL UNIQUE, hashedPassword VARCHAR(255) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE, 
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    hashedPassword VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
 
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
